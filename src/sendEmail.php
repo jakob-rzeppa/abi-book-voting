739bfee105d7bf55e5f98373fa7043871c928768
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-03-22 10:46:18 +0100

    implemented sendEmail to send Emails from abi2025igsff Google Account

---
+++ src/sendEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'vendor/autoload.php';
+
+function sendEmail($address)
+{
+    $send = false;
+
+    $mail = new PHPMailer(true);
+
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = "smtp.gmail.com";
+    $mail->Mailer = "smtp";
+    $mail->Port = 587;
+    $mail->Username = "abiigsff2025@gmail.com";
+    // $mail->Password = "";
+
+    //sender information
+    $mail->setFrom('abiigsff2025@gmail.com', 'abi-2025-igsff');
+
+    //receiver email address and name
+    $mail->addAddress($address, $address);
+
+    $mail->isHTML(false);
+
+    $mail->Subject = 'Abi Buch Abstimmungen';
+    $mail->Body    = "Clicke auf diesen Link, um an der Abstimmung teilzunehmen";
+
+    // Attempt to send the email
+    if (!$mail->send()) {
+        $send = false;
+    } else {
+        $send = true;
+    }
+
+    $mail->smtpClose();
+
+    return $send;
+}

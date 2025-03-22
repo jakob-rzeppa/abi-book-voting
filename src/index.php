d2e3009637f4c37e83667cc9195c11e949015c6d
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-03-22 10:47:37 +0100

    created a form to input the Email and get a Email

--- src/index.php
+++ src/index.php
@@ -1,2 +1,48 @@
-<?php
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abi Buch Abstimmungen</title>
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <label>Email:</label>
+        <input type="email" id="email" name="email">
+        <input type="submit" value="Send Email">
+    </form>
+    <?php
+    include 'send_email.php';
+
+    $email = $_POST['email'];
+
+    if (isset($email)) {
+        if (empty($email)) {
+            echo 'Bitte gib eine Email Adresse ein';
+            exit;
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'Invalide Email Adresse';
+            exit;
+        }
+
+        if ($email !== 'rzeppa.jakob@gmail.com') { // TODO change email address to igsff-bs.de
+            echo 'Du musst eine IGSFF Email Adresse verwenden';
+            exit;
+        }
+
+        $send = sendEmail($email);
+
+        if ($send) {
+            echo 'Email wurde versendet. Bitte überprüfe dein Postfach und Spam-Ordner';
+        } else {
+            echo 'Email konnte nicht versendet werden';
+        }
+    }
+    ?>
+</body>
+
+</html>

0cac95bbafd7ed24dab43e4c2e88d45be6e44e84
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-03-25 20:12:22 +0100

    Add voted table creation and voting functions for user interactions

---
+++ src/db/votedDb.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'db.php';
+
+function createVotedTable()
+{
+    global $conn;
+
+    $sql = "CREATE TABLE IF NOT EXISTS voted (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        user_id INT(6) UNSIGNED,
+        question_id INT(6) UNSIGNED,
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        FOREIGN KEY (question_id) REFERENCES question(id)
+    )";
+
+    $conn->exec($sql);
+}
+
+function insertVoted($user_id, $question_id)
+{
+    global $conn;
+
+    $sql = "INSERT INTO voted (user_id, question_id) VALUES ($user_id, $question_id)";
+
+    $conn->exec($sql);
+}
+
+function getAlreadyVotedQuestions($user_id)
+{
+    global $conn;
+
+    $sql = "SELECT question_id FROM voted WHERE user_id = $user_id";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}

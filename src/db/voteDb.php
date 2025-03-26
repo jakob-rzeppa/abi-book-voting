36c322c5207e3c2e4bd7b931bca50fbeacc3d1b0
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-03-26 21:39:00 +0100

    add voteDb implementation of teacher votes

--- src/db/voteDb.php
+++ src/db/voteDb.php
@@ -14,11 +14,21 @@ function getVotes($question_id)
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function insertVote($student_id, $question_id)
+function insertVote($student_id, $teacher_id, $question_id)
 {
     global $conn;
 
-    $sql = "INSERT INTO vote (student_id, question_id) VALUES ($student_id, $question_id)";
+    if ($student_id && $teacher_id) {
+        return;
+    }
+
+    if ($student_id) {
+        $sql = "INSERT INTO vote (student_id, question_id) VALUES ($student_id, $question_id)";
+    } else if ($teacher_id) {
+        $sql = "INSERT INTO vote (teacher_id, question_id) VALUES ($teacher_id, $question_id)";
+    } else {
+        return;
+    }
 
     $conn->exec($sql);
 }

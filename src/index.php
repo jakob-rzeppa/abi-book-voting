0ef4a7b8c2687563ed868e9f316756f14e749ef6
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-04-11 10:27:53 +0200

    move css to seperate file

--- src/index.php
+++ src/index.php
@@ -22,71 +22,7 @@ require_once('./util/sendEmail.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Abi Buch Abstimmungen</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 300px;
-            text-align: center;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-        }
-
-        label {
-            margin-bottom: 10px;
-            font-weight: bold;
-        }
-
-        input[type="email"] {
-            padding: 10px;
-            margin-bottom: 20px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        input[type="submit"] {
-            padding: 10px;
-            background-color: #28a745;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #218838;
-        }
-
-        a {
-            display: block;
-            margin-top: 20px;
-            color: #007bff;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-
-        .message {
-            margin-top: 20px;
-            color: red;
-        }
+        <?php include "./css/index.css" ?>
     </style>
 </head>
 
